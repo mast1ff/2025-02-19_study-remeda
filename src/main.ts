92acd6160487b09d7500250b5453cf7c6d14e769
mast1ff <mast1ff929@gmail.com>
2025-02-25 23:48:17 +0900

    updated

--- src/main.ts
+++ src/main.ts
@@ -60,8 +60,10 @@ console.log(MODIFIED_SAMPLE_DATA_3);
 const allPass_isEven = (x: number) => x % 2 === 0;
 const allPass_isPositive = (x: number) => x > 0;
 const allPass_fns = [allPass_isEven, allPass_isPositive];
-console.log(R.allPass(2, allPass_fns));
-console.log(R.allPass(3, allPass_fns));
+
+const allPass = R.allPass(allPass_fns);
+console.log(allPass(2));
+console.log(allPass(3));
 
 /**
  * anyPass
@@ -70,29 +72,71 @@ console.log(R.allPass(3, allPass_fns));
 const anyPass_isEven = (x: number) => x % 2 === 0;
 const anyPass_isPositive = (x: number) => x > 0;
 const anyPass_fns = [anyPass_isEven, anyPass_isPositive];
-console.log(R.anyPass(2, anyPass_fns));
-console.log(R.anyPass(3, anyPass_fns));
+const anyPass = R.anyPass(anyPass_fns);
+console.log(anyPass(2));
+console.log(anyPass(3));
 
 /**
  * chunk
  * 配列を指定したサイズで分割します。
  */
-console.log(R.chunk([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+const chunk = R.chunk(3);
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
 /**
  * concat
  * 配列を結合します。
  */
-console.log(R.concat([1, 2, 3], [4, 5, 6]));
+const concat = R.concat([1, 2, 3]);
+console.log(concat([4, 5, 6]));
 
 /**
  * countBy
  * 配列の要素をカウントします。
  */
-console.log(R.countBy(["a", "b", "c", "B", "A", "a"], R.toLowerCase()));
+const countByResult = R.countBy(
+  ["a", "b", "c", "B", "A", "a"],
+  R.toLowerCase()
+);
+console.log(countByResult);
 
 /**
  * difference
  * 2つの配列の差分を取得します。
  */
-console.log(R.pipe([1, 2, 3, 4], R.difference([3, 4, 5])));
+const differenceResult = R.pipe([1, 2, 3, 4], R.difference([3, 4, 5]));
+console.log(differenceResult);
+
+/**
+ * drop
+ * 配列の先頭から指定した数の要素を削除します。
+ */
+const dropResult = R.drop([1, 2, 3, 4, 5], 2);
+console.log(dropResult);
+
+/**
+ * dropFirstBy
+ * 条件に一致する最初の要素を削除します。
+ */
+const dropFirstBy = R.pipe(
+  ["a", "aaaa", "aa", "aaa"],
+  R.dropFirstBy(2, (x) => x.length)
+);
+console.log(dropFirstBy);
+
+/**
+ * dropLast
+ * 配列の末尾から指定した数の要素を削除します。
+ */
+const dropLast = R.dropLast(2);
+console.log(dropLast([1, 2, 3, 4, 5]));
+
+/**
+ * dropLastWhile
+ * 条件に一致する要素を末尾から削除します。
+ */
+const dropLastWhile = R.pipe(
+  [1, 2, 3, 4, 5],
+  R.dropLastWhile((x) => x > 3)
+);
+console.log(dropLastWhile);
